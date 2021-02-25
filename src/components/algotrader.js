c05e48d3d372ca7f5a697e06cefa2ebafe8680b3
Joseph Mattern <52804088+jcm005@users.noreply.github.com>
2021-02-24 20:33:46 -0500

    Update algotrader.js

--- src/components/algotrader.js
+++ src/components/algotrader.js
@@ -10,6 +10,7 @@ const Algotrader = () => {
                     <a className='project2 link black dim condiv '
                     href='https://github.com/jcm005/QuantitativeTrader'>
                     <p className='p10'>Algorithmic trading</p>
+                    <p className=' mt3 tc'>Develop algorithms and strategies for real time trading based on many market indicators.</p>
                     </a>
                 </figcaption>
             </figure>
